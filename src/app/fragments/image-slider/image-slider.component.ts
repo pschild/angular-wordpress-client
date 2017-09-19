ee7ade3c5469916b0d03df629087f87391570f85
pschild <philippe.schild@gmail.com>
2017-09-19 16:14:45 +0200

    get correct image url for image grid

--- src/app/fragments/image-slider/image-slider.component.ts
+++ src/app/fragments/image-slider/image-slider.component.ts
@@ -17,6 +17,14 @@ export class ImageSliderComponent implements OnInit {
     ngOnInit() {
     }
 
+    getSourceUrl(item) {
+        if (item.media_details.height > item.media_details.width) {
+            return item.media_details.sizes.image_grid_portrait.source_url;
+        } else {
+            return item.media_details.sizes.image_grid_landscape.source_url;
+        }
+    }
+
     setActiveIndex(newIndex) {
         if (newIndex === this.items.length || newIndex < 0) {
             return;
