352dad0356a942d17d54f81f20e16e30d5e87534
pschild <philippe.schild@gmail.com>
2017-10-13 12:59:50 +0200

    Use library ngwt-wordpress-templates instead of templates and fragments directly defined within the application.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -8,14 +8,16 @@ import {HeaderComponent} from "./header/header.component";
 import {PageService} from "./page/page.service";
 import {PostService} from "./post/post.service";
 import {FormsModule} from "@angular/forms";
-import {TplGalleryModule} from "../templates/tpl-gallery/tpl-gallery.module";
-import {TplTextModule} from "../templates/tpl-text/tpl-text.module";
-import {TplTextMediaSliderModule} from "../templates/tpl-text-media-slider/tpl-text-media-slider.module";
-import {TplTextGalleryModule} from "../templates/tpl-text-gallery/tpl-text-gallery.module";
-import {TplProjectsModule} from "../templates/tpl-projects/tpl-projects.module";
-import {TplPostsModule} from "../templates/tpl-posts/tpl-posts.module";
-import {TplTimelineModule} from "../templates/tpl-timeline/tpl-timeline.module";
-import {TplCodeblocksModule} from "../templates/tpl-codeblocks/tpl-codeblocks.module";
+import {
+    NGWT_CONFIG, NgwtConfig, TplGalleryModule, TplPostsModule, TplTextGalleryModule, TplTextMediaSliderModule,
+    TplTextModule
+} from "ng-wordpress-templates";
+import {environment} from "../../environments/environment";
+
+const libConfig: NgwtConfig = {
+    apiUrl: environment.apiUrl,
+    staticSharerUrl: environment.staticSharerUrl
+};
 
 @NgModule({
     imports: [
@@ -24,14 +26,11 @@ import {TplCodeblocksModule} from "../templates/tpl-codeblocks/tpl-codeblocks.mo
 
         NavigationModule,
 
-        TplGalleryModule,
-        TplTextModule,
-        TplTextMediaSliderModule,
-        TplTextGalleryModule,
-        TplProjectsModule,
-        TplPostsModule,
-        TplTimelineModule,
-        TplCodeblocksModule
+        TplTextModule.forRoot(),
+        TplPostsModule.forRoot(),
+        TplTextGalleryModule.forRoot(),
+        TplTextMediaSliderModule.forRoot(),
+        TplGalleryModule.forRoot()
     ],
     declarations: [
         TemplateComponent,
@@ -46,7 +45,11 @@ import {TplCodeblocksModule} from "../templates/tpl-codeblocks/tpl-codeblocks.mo
     ],
     providers: [
         PageService,
-        PostService
+        PostService,
+        {
+            provide: NGWT_CONFIG,
+            useValue: libConfig
+        }
     ]
 })
 export class CoreModule {
